2577f2086a7cd04a23e3c94a1f4d86d2a58438a0
Avi Keinan <avik@doit-intl.com>
2020-09-12 22:50:12 +0300

    added comments

--- janus.py
+++ janus.py
@@ -6,13 +6,13 @@ import sys
 
 
 def get_metadata(path: str, parameter: str):
-    # use format instead of f-type to support python version before 3.7
+    # Use .format() instead of f-type to support python version before 3.7
     metadata_url = 'http://metadata.google.internal/computeMetadata/v1/{}/{}'.format(path, parameter)
     headers = {'Metadata-Flavor': 'Google'}
-    # execute metadata request
+    # execute http metadata request
     try:
         meta_request = requests.get(metadata_url, headers=headers)
-    except requests.exceptions.RequestException as e:  # This is the correct syntax
+    except requests.exceptions.RequestException as e:
         raise SystemExit(e)
 
     if meta_request.ok:
@@ -22,22 +22,22 @@ def get_metadata(path: str, parameter: str):
 
 
 if __name__ == '__main__':
+    # Get aws arn from command line argument
     try:
         aws_role_arn = sys.argv[1]
     except IndexError:
         print('Please specify AWS arn role:\n{} arn:aws:iam::account-id:role/role-name'.format(sys.argv[0]))
         exit(0)
 
+    # Get variables from the metadata server
     instance_name = get_metadata('instance', 'name')
     project_id = get_metadata('project', 'project-id')
     project_and_instance_name = '{}.{}'.format(project_id, instance_name)
     token = get_metadata('instance', 'service-accounts/default/identity?format=standard&audience=gcp')
-    # Set up the URL to request
 
-    # Turn the token into AWS credentials
+    # Assume role using gcp service account token
     sts = boto3.client('sts', aws_access_key_id='', aws_secret_access_key='')
 
-    # assume role using gcp service account token
     res = sts.assume_role_with_web_identity(
         RoleArn=aws_role_arn,
         WebIdentityToken=token,
